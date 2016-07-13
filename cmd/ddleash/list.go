9d03567f707dcf0afe32605fbe9fd3e695ccf718
Matthieu Hauglustaine <matthieu.hauglustaine@dailymotion.com>
2016-07-13 11:18:20 +0200

    list: implement list function and support listing metrics.

--- cmd/ddleash/list.go
+++ cmd/ddleash/list.go
@@ -1,9 +1,18 @@
-package cmd
+package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	"github.com/matthauglustaine/ddleash"
+)
+
+const (
+	window = 3600
 )
 
 // listCmd represents the list command
@@ -19,5 +28,44 @@ func init() {
 }
 
 func runListCmd(cmd *cobra.Command, args []string) {
-	fmt.Println("list: hello world!")
+	item := "metrics"
+	listArgs := []string{}
+	if len(args) > 0 {
+		item = args[0]
+		listArgs = args[1:]
+	}
+
+	listFunc, ok := map[string]func(*ddleash.Client, []string) error{
+		"metrics": listMetrics,
+	}[item]
+
+	if !ok {
+		fmt.Printf("Unknown item to list: %q\n", item)
+		os.Exit(-1)
+	}
+
+	client := ddleash.New(ddleash.Account{
+		Team:     viper.GetString("datadog.team"),
+		User:     viper.GetString("datadog.user"),
+		Password: viper.GetString("datadog.password"),
+	})
+
+	if err := listFunc(client, listArgs); err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
+
+func listMetrics(client *ddleash.Client, _ []string) error {
+	if err := client.Login(); err != nil {
+		return err
+	}
+
+	metrics, err := client.FetchAllMetricNames(window)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(strings.Join(metrics, "\n"))
+	return nil
 }
